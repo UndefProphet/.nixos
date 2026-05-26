20bd5707a49ef8f30f36fd18e898fb85a36c14b7
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-05-26 18:21:32 +0200

    Change colorscheme to gruvbox

--- modules/environment/stylix.nix
+++ modules/environment/stylix.nix
@@ -32,10 +32,11 @@
       enable = true;
       autoEnable = true;
       polarity = "dark";
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+      # base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest-dark-hard.yaml";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 
       opacity = let
-        opacity = 0.75;
+        opacity = 0.85;
       in {
         applications = opacity;
         desktop = opacity;
--- modules/packages/neovim/_config/plugins/colorscheme.nix
+++ modules/packages/neovim/_config/plugins/colorscheme.nix
@@ -26,7 +26,7 @@
   colorschemes = {
 
     everforest = {
-      enable = true;
+      enable = false;
       settings = {
         terminal_colors = lib.mkDefault true; # add neovim terminal colors
         undercurl = lib.mkDefault true;
@@ -52,32 +52,32 @@
       };
     };
 
-  };
 
-  # colorschemes.gruvbox = {
-  #   enable = lib.mkDefault true;
-  #   settings = {
-  #     terminal_colors = lib.mkDefault true; # add neovim terminal colors
-  #     undercurl = lib.mkDefault true;
-  #     underline = lib.mkDefault true;
-  #     bold = lib.mkDefault true;
-  #     italic = {
-  #       strings = lib.mkDefault true;
-  #       emphasis = lib.mkDefault true;
-  #       comments = lib.mkDefault true;
-  #       operators = lib.mkDefault false;
-  #       folds = lib.mkDefault true;
-  #     };
-  #     strikethrough = lib.mkDefault true;
-  #     invert_selection = lib.mkDefault false;
-  #     invert_signs = lib.mkDefault false;
-  #     invert_tabline = lib.mkDefault false;
-  #     inverse = lib.mkDefault true; # invert background for search, diffs, statuslines and errors
-  #     contrast = lib.mkDefault ""; # can be "hard", "soft" or empty string
-  #     palette_overrides = {};
-  #     overrides = {};
-  #     dim_inactive = lib.mkDefault false;
-  #     transparent_mode = lib.mkDefault true;
-  #   };
-  # };
+    gruvbox = {
+      enable = lib.mkDefault true;
+      settings = {
+        terminal_colors = lib.mkDefault true; # add neovim terminal colors
+        undercurl = lib.mkDefault true;
+        underline = lib.mkDefault true;
+        bold = lib.mkDefault true;
+        italic = {
+          strings = lib.mkDefault true;
+          emphasis = lib.mkDefault true;
+          comments = lib.mkDefault true;
+          operators = lib.mkDefault false;
+          folds = lib.mkDefault true;
+        };
+        strikethrough = lib.mkDefault true;
+        invert_selection = lib.mkDefault false;
+        invert_signs = lib.mkDefault false;
+        invert_tabline = lib.mkDefault false;
+        inverse = lib.mkDefault true; # invert background for search, diffs, statuslines and errors
+        contrast = lib.mkDefault ""; # can be "hard", "soft" or empty string
+        palette_overrides = {};
+        overrides = {};
+        dim_inactive = lib.mkDefault false;
+        transparent_mode = lib.mkDefault true;
+      };
+    };
+  };
 }
