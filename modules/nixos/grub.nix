d656a0d4588e90dc338f912a86df3151834307a3
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-06-06 18:21:22 +0200

    Make grub removable

--- modules/nixos/grub.nix
+++ modules/nixos/grub.nix
@@ -2,25 +2,16 @@
   flake.modules.nixos.grub= {
 
     boot.loader = {
-      efi.canTouchEfiVariables = true;
       grub = {
         enable = true;
         device = "nodev";
         efiSupport = true;
+        efiInstallAsRemovable = true;
         useOSProber = true;
         gfxmodeEfi = "3440x1440";
         gfxmodeBios = "3440x1440";
         splashMode = "normal";
       };
     };
-
-
-    # boot = {
-    #   initrd.systemd.enable = false; # Required for swapfile hibernationpep
-    #   loader = {
-    #     efi.canTouchEfiVariables = true;
-    #     systemd-boot.enable = true;
-    #   };
-    # };
   };
 }
