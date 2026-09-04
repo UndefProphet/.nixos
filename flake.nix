fc234751e616cee6aa2d551542c4237341f520bc
Prophet <206461177+UndefinedProphet@users.noreply.github.com>
2026-09-04 03:11:08 +0200

    Replace native flake inputs with tack

--- flake.nix
+++ flake.nix
@@ -1,121 +1,13 @@
-# D o  N o t  E d i t o  O r  I  K i l l o  B i l l u !
 {
-  description = "Potato";
+  description = "NixOS configuration";
+  outputs = args@{ self, ... }: let
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree 
-    [
-      ./modules
-      ./hosts
+    # Tack as input manager with lazy evaluation
+    inputs = (import ./inputs) { overrides = args.tackOverrides or { }; };
+    self' = self // { inputs = inputs; }; # to make tack compatiable with flake-parts
+
+  in inputs.flake-parts.lib.mkFlake { inherit inputs; self = self'; } (inputs.import-tree [
+      ./hosts # Hardware based configurations
+      ./modules # Modules
     ]);
-
-  inputs = {
-    base16 = {
-      type = "github";
-      owner = "SenchoPens";
-      repo = "base16.nix";
-    };
-    disko = {
-      type = "github";
-      owner = "nix-community";
-      repo = "disko";
-      ref = "master";
-    };
-    flake-file.url = "github:vic/flake-file";
-    flake-parts = {
-      type = "github";
-      owner = "hercules-ci";
-      repo = "flake-parts";
-    };
-    glide-browser = {
-      url = "github:glide-browser/glide.nix";
-      inputs = {
-        home-manager.follows = "home-manager";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    home-manager = {
-      type = "github";
-      owner = "nix-community";
-      repo = "home-manager";
-      ref = "master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland = {
-      type = "github";
-      owner = "hyprwm";
-      repo = "Hyprland";
-      ref = "main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    import-tree = {
-      type = "github";
-      owner = "vic";
-      repo = "import-tree";
-    };
-    niri = {
-      url = "github:niri-wm/niri";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    niri-flake = {
-      type = "github";
-      owner = "myume";
-      repo = "niri-flake";
-      ref = "blur";
-    };
-    nix-index-database = {
-      type = "github";
-      owner = "nix-community";
-      repo = "nix-index-database";
-      ref = "main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixcord = {
-      type = "github";
-      owner = "FlameFlag";
-      repo = "nixcord";
-      ref = "main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-core = {
-      type = "github";
-      owner = "manic-systems";
-      repo = "nixos-core";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-credentials = {
-      url = "git+ssh://git@github.com/UndefProphet/.nixos.credentials";
-      flake = false;
-    };
-    nixpkgs = {
-      type = "github";
-      owner = "nixos";
-      repo = "nixpkgs";
-      ref = "nixos-unstable";
-    };
-    nixvim.url = "github:nix-community/nixvim";
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      type = "github";
-      owner = "Mic92";
-      repo = "sops-nix";
-      ref = "master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    spicetify-nix = {
-      type = "github";
-      owner = "Gerg-L";
-      repo = "spicetify-nix";
-      ref = "master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      type = "github";
-      owner = "nix-community";
-      repo = "stylix";
-      ref = "master";
-    };
-  };
 }
